7fae2c2b0a0b25edb193eee1dc1dd891fd42d89c
shinji0320 <shinji.blue.one@gmail.com>
2021-04-03 10:13:08 +0900

    環境変数追加

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,4 +1,13 @@
 class HelloController < ApplicationController
   def index
   end
+
+  private
+ 
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
 end
